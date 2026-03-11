85380dd3bfb08274a9fb5d08a237ae7cb2411d8a
Vladyslav <vladzvezdaev@gmail.com>
2026-03-11 20:36:16 +0200

    add: stabilyty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Hero } from "@/widgets/Hero";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  await new Promise((res) => setTimeout(res, 700));
+  await new Promise((res) => setTimeout(res, 1000));
   return (
     <main>
       <Hero />
