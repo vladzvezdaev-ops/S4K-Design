a9e56b2c7dcb5f68490beb10a75a465c9f7db83d
Vladyslav <vladzvezdaev@gmail.com>
2026-03-18 00:14:43 +0200

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Header } from "@/widgets/Header";
 import { Footer } from "@/widgets/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.scss";
 
@@ -42,6 +43,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
